184dc6aa46cf6e68cb1d342ca77e3bf3f8ac640a
Connor McFadden <connormcfadden7@gmail.com>
2018-12-11 15:25:58 +0000

    Minor margin/padding fixes

--- app/src/main/java/com/curriculumvitae/android/data/api/ApiCurriculumVitae.kt
+++ app/src/main/java/com/curriculumvitae/android/data/api/ApiCurriculumVitae.kt
@@ -6,6 +6,6 @@ import retrofit2.http.GET
 
 interface ApiCurriculumVitae {
 
-    @GET("/ConnorMcFadden7/21fae1cf67de98457d0e4bb968ecd606/raw/b6cc2e4554210bcc7f6ed35237f36c52e70b1c57/connor_mcfadden_cv")
+    @GET("/ConnorMcFadden7/21fae1cf67de98457d0e4bb968ecd606/raw/ccb1ce84ac79921aa895ddb1d611d80426de9802/connor_mcfadden_cv")
     fun getCurriculumVitae(): Observable<CurriculumVitae>
 }
