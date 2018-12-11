aa10405813917601a9c62bda31586f1ebab3947a
Connor McFadden <connormcfadden7@gmail.com>
2018-12-11 15:28:52 +0000

    Added some documentation to ViewSlices

--- app/src/main/java/com/curriculumvitae/android/view/viewslice/ViewSlice.kt
+++ app/src/main/java/com/curriculumvitae/android/view/viewslice/ViewSlice.kt
@@ -4,6 +4,11 @@ import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.view.View
 
+/**
+ * ViewSlice interface responsible for initialising sub ViewSlices. The purpose of a ViewSlice is to extract
+ * complex view logic/binding from Activities or Fragments to reduce boilerplate code. ViewSlices also make view
+ * components much easier to test.
+ */
 interface ViewSlice : LifecycleObserver {
 
     fun init(lifeCycle: Lifecycle, view: View)
--- app/src/main/java/com/curriculumvitae/android/view/viewslice/ViewSliceHelper.kt
+++ app/src/main/java/com/curriculumvitae/android/view/viewslice/ViewSliceHelper.kt
@@ -4,6 +4,9 @@ import android.arch.lifecycle.Lifecycle
 import android.view.View
 import javax.inject.Inject
 
+/**
+ * Class to help initialise a list of viewSlices passed in
+ */
 class ViewSliceHelper @Inject constructor() {
 
     fun initWidget(lifecycle: Lifecycle, view: View, vararg viewSlices: ViewSlice) {
